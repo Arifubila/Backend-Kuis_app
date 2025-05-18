8f97a814d66bec1827f16a77160700452d85b5f9
Arifubila <arifubila5789@gmail.com>
2025-05-18 17:56:16 +0700

    chore: remove src folder from init/server

--- src/routes/quizRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const quizController = require("../controllers/quizController");
-const verifyRole = require("../middleware/authMiddleware");
-
-router.post("/create", verifyRole(["admin"]), quizController.createQuiz);
-router.get("/", quizController.getAllQuizzes);
-
-module.exports = router;
