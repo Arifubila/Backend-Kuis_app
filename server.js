a7500e1dec6772bf8345e2d460806ba5b46b6451
Arifubila <arifubila5789@gmail.com>
2025-05-18 15:32:13 +0700

    init: setup server.js for kuis app

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const connectDB = require("./src/config/db");
+const authRoutes = require("./src/routes/authRoutes");
+
+connectDB();
+const app = express();
+app.use(express.json());
+
+app.use("/api", authRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Quiz App Backend is running...");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
