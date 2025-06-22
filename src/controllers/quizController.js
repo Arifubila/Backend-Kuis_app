83698f621221382166d2335d88f6b4742241412b
Arifubila <arifubila5789@gmail.com>
2025-06-22 17:55:25 +0700

    Update quiz Code

--- src/controllers/quizController.js
+++ src/controllers/quizController.js
@@ -13,8 +13,30 @@ exports.createQuiz = async (req, res) => {
 
 exports.getAllQuizzes = async (req, res) => {
   try {
-    const quizzes = await Quiz.find({}, { "questions.answer": 0 });
+    const { category } = req.query;
+
+    let filter = {};
+    if (category) {
+      filter.category = category; // filter jika ada kategori
+    }
+
+    const quizzes = await Quiz.find(filter);
     res.json(quizzes);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Ambil kuis berdasarkan kategori
+exports.getQuizByCategory = async (req, res) => {
+  const { category } = req.query;
+
+  try {
+    const quiz = await Quiz.find({ category }); // misal "matematika"
+    if (!quiz || quiz.length === 0) {
+      return res.status(404).json({ message: "Quiz kategori tidak ditemukan" });
+    }
+    res.json(quiz);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
