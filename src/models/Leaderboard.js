96b759066fab6eb0665861c2fcced22139502ac9
Arifubila <arifubila5789@gmail.com>
2025-06-23 12:14:36 +0700

    Fix Bug Leaderboard duplicat

--- src/models/Leaderboard.js
+++ src/models/Leaderboard.js
@@ -1,10 +1,11 @@
+// models/Leaderboard.js
 const mongoose = require("mongoose");
 
 const leaderboardSchema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: "User", // harus sesuai dengan nama model
-    required: true,
+    ref: "User",
+    unique: true, // ⬅️ WAJIB! Agar user tidak bisa punya skor lebih dari 1
   },
   score: {
     type: Number,
